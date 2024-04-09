25c355b9cbeaf49ea21c54cb015fe8764897b9ab
Leonov Stanislav <ragioniere1980@gmail.com>
2024-04-09 10:25:09 +0300

    instagram card

--- app/src/main/java/dev/stas/instagramcard/ui/InstagramFrofileCard.kt
+++ app/src/main/java/dev/stas/instagramcard/ui/InstagramFrofileCard.kt
@@ -13,11 +13,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,16 +29,16 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Devices.TABLET
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.stas.instagramcard.ManViewModel
 import dev.stas.instagramcard.R
-import dev.stas.instagramcard.ui.theme.InstagramCardTheme
 
 @Composable
-fun InstagramProfileCard() {
+fun InstagramProfileCard(
+    viewModel: ManViewModel
+) {
+    val isFollowed: State<Boolean> = viewModel.isFollowed.observeAsState(false)
     Card(
         modifier = Modifier
             .padding(8.dp),
@@ -68,8 +71,7 @@ fun InstagramProfileCard() {
                 UserStatistics(title = "Followers", value = "436M")
                 UserStatistics(title = "Following", value = "76")
             }
-            Column(
-            ) {
+            Column {
                 Text(
                     text = "Instagram",
                     fontSize = 36.sp,
@@ -80,18 +82,41 @@ fun InstagramProfileCard() {
                     fontSize = 14.sp,
                 )
                 Text(
-                    text = "www.facebook.com/ololo",
+                    text = "www.facebook.com/boo",
                     fontSize = 14.sp,
                 )
-                Button(
-                    onClick = {},
-                    border = BorderStroke(1.dp, Color.Black)
-                ){
-                    Text(text = "Follow")
+                FollowButton(isFollowed.value){
+                    viewModel.changeFollowingStatus()
                 }
             }
-
         }
+    }
+}
+
+@Composable
+fun FollowButton(
+    isFollowed: Boolean,
+    clickListener: () -> Unit
+) {
+    Button(
+        onClick = {
+            clickListener()
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isFollowed) {
+                MaterialTheme.colorScheme.primary.copy(alpha = 0.4f)
+            } else {
+                MaterialTheme.colorScheme.primary
+            }
+
+        )
+    ) {
+        val text = if (isFollowed) {
+            "Unfollow"
+        } else {
+            "Follow"
+        }
+        Text(text = text)
     }
 }
 
@@ -115,25 +140,5 @@ private fun UserStatistics(
             text = title,
             fontWeight = FontWeight.Bold
         )
-    }
-}
-
-@Preview(name = "Tablet", device = Devices.PHONE, showSystemUi = true)
-@Composable
-fun PreviewCardLight() {
-    InstagramCardTheme(
-        darkTheme = false
-    ) {
-        InstagramProfileCard()
-    }
-}
-
-@Preview(name = "Tablet", device = TABLET, showSystemUi = true)
-@Composable
-fun PreviewCardDark() {
-    InstagramCardTheme(
-        darkTheme = true
-    ) {
-        InstagramProfileCard()
     }
 }
